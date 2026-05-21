d1b2d273b33f7fbb847143d6b260e46c885f3d1c
izzy <jakktan@gmail.com>
2026-05-20 20:11:55 -0500

    made further comments in the application factory

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -6,19 +6,25 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
+    # to start the app in the current python file and use the instance folder for configurations
     bookshelf = Flask(__name__, instance_relative_config=True)
+
+    """ 
+    configurations
+    
+    """
     bookshelf.config.from_mapping(
-        #for 
+        #for development only, need to change for production
         SECRET_KEY=os.environ.get('SECRET_KEY','dev'),
         #stores the database in the instance folder
         DATABASE=os.path.join(bookshelf.instance_path, 'bookshelf.sqlite'),
     )
 
     if test_config is None:
-        #to update the secret key for deployment
+        # if testing, the tests from config.py are used
         bookshelf.config.from_pyfile('config.py', silent=True)
     else:
-        #configurations for testing
+        #configurations for testing if test_config is true
         bookshelf.config.update(test_config)
 
     #makes the instance folder if not present.
@@ -28,15 +34,19 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #to initialize the database with bookshelf
+    """
+    database connections and blueprints
+
+    """
+    #to initialize the database
     from . import db
     db.init_app(bookshelf)
 
-   # to initialize the schema with bookshelf
+   # to initialize the schema
     from . import schema
     schema.init_app(bookshelf)
 
-    #registers blueprints with bookshelf
+    # to register the blueprints
     from . import auth
     from . import books
     from . import api
